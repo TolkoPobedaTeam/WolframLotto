2bbb9327589c2b53c4d8909f0643ef9a001be7c7
yak80 <aleksandr.yakushev@gmail.com>
2014-12-07 16:37:58 +0400

    change storage

--- server/classes/db.php
+++ server/classes/db.php
@@ -2,29 +2,34 @@
 
 class Db
 {
-private $filedir='/../filedb/';
+//private $filedir='/../filedb/';
+private $alldata;
 
 private function getPath($serverid,$type)
 {
-return __DIR__.$this->filedir."server$serverid.$type.json";
+//return __DIR__.$this->filedir."server$serverid.$type.json";
 }
 
 public function readData($serverid,$type)
 {
-$filename=$this->getPath($serverid,$type);
+if(empty($this->alldata[$serverid][$type])) $this->alldata[$serverid][$type]=array();
+return $this->alldata[$serverid][$type];
+
+/*$filename=$this->getPath($serverid,$type);
 $data=array();
 if(file_exists($filename)) 
 	{
 	$data=json_decode(file_get_contents($filename),true);
 	}
-return $data;
+return $data;*/
 }
 
 
 public function writeData($serverid,$type,$data)
 {
-$filename=$this->getPath($serverid,$type);
-file_put_contents($filename,json_encode($data));
+$this->alldata[$serverid][$type]=$data;
+//$filename=$this->getPath($serverid,$type);
+//file_put_contents($filename,json_encode($data));
 }
 
 }
