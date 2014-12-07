801f5efb48ffbdb200fd5808e8f2943acb52df00
yak80 <aleksandr.yakushev@gmail.com>
2014-12-08 01:07:48 +0400

    iterations

--- server/classes/wolframApi.php
+++ server/classes/wolframApi.php
@@ -32,24 +32,36 @@ return $items[$line];
 
 public function get($word)
 {
-$code=file_get_contents("http://www.wolframalpha.com/input/autocomplete.jsp?qr=0&i=$word");
-$quick=json_decode($code,true);
-$mainimg=$quick["results"][0]['summaryBoxPath'];
+foreach(array(1,2,3) as $iteration)
+	{
+	$code=file_get_contents("http://www.wolframalpha.com/input/autocomplete.jsp?qr=0&i=$word");
+	$quick=json_decode($code,true);
+	$mainimg=$quick["results"][0]['summaryBoxPath'];
+	if(!empty($mainimg)) break;
+	}
+
+
 //echo "<img src='http://www.wolframcdn.com/summaryboxes/$img/img/mainimage.gif' />";
-$code=file_get_contents("http://api.wolframalpha.com/v2/query?input=$word&appid=5688U4-84L7K4AK23");
-$xml = simplexml_load_string($code);
-$json = json_encode($xml);
-$data = json_decode($json,TRUE);
-$struct=array();
-foreach($data["pod"] as $k=>$pod)
-    {
-    $id=strtolower($pod["@attributes"]["id"]);
-    $plain=$pod["subpod"]["plaintext"];
-    $img=$pod["subpod"]["img"]["@attributes"]["src"];
-    $imgw=$pod["subpod"]["img"]["@attributes"]["width"];
-    $imgh=$pod["subpod"]["img"]["@attributes"]["height"];
-    $struct[$id]=array("plain"=>$plain,"img"=>$img);
-    }
+foreach(array(1,2,3) as $iteration)
+	{
+	$code=file_get_contents("http://api.wolframalpha.com/v2/query?input=$word&appid=5688U4-84L7K4AK23");
+	$xml = simplexml_load_string($code);
+	$json = json_encode($xml);
+	$data = json_decode($json,TRUE);
+	$struct=array();
+	foreach($data["pod"] as $k=>$pod)
+		{
+		$id=strtolower($pod["@attributes"]["id"]);
+		$plain=$pod["subpod"]["plaintext"];
+		$img=$pod["subpod"]["img"]["@attributes"]["src"];
+		$imgw=$pod["subpod"]["img"]["@attributes"]["width"];
+		$imgh=$pod["subpod"]["img"]["@attributes"]["height"];
+		$struct[$id]=array("plain"=>$plain,"img"=>$img);
+		}
+	if(!empty($struct["demographicproperties:countrydata"]["plain"])
+		and (!empty($struct["capitalcity:countrydata"]["plain"]))
+		and (!empty($struct["geographicproperties:countrydata"]["plain"]))) break;
+	}
 $data=array();
 $data[]=$this->parseStr($this->getLineByNum($struct["demographicproperties:countrydata"]["plain"]));
 $data[]=$this->parseStr($this->getLineByNum($struct["capitalcity:countrydata"]["plain"]),"Capital");
